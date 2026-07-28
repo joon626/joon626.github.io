91a4028569048fd9f924399bcdfc076bf291d63f
gplation <hetosi06@naver.com>
2026-07-28 16:38:43 +0900

    Add active section animation

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const siteNav = document.querySelector('#site-nav');
+const navLinks = siteNav ? [...siteNav.querySelectorAll('a[href^="#"]')] : [];
+const selectableSections = navLinks
+  .map((link) => document.querySelector(link.getAttribute('href')))
+  .filter(Boolean);
+
+function selectSection(sectionId) {
+  navLinks.forEach((link) => {
+    const isSelected = link.getAttribute('href') === `#${sectionId}`;
+    link.toggleAttribute('aria-current', isSelected);
+  });
+
+  selectableSections.forEach((section) => {
+    const isSelected = section.id === sectionId;
+    section.classList.remove('is-selected');
+    if (isSelected) requestAnimationFrame(() => section.classList.add('is-selected'));
+  });
+}
 
 if (menuToggle && siteNav) {
   menuToggle.addEventListener('click', () => {
@@ -9,8 +26,14 @@ if (menuToggle && siteNav) {
 
   siteNav.addEventListener('click', (event) => {
     if (event.target.matches('a')) {
+      const targetId = event.target.getAttribute('href').slice(1);
+      if (selectableSections.some((section) => section.id === targetId)) selectSection(targetId);
       siteNav.classList.remove('is-open');
       menuToggle.setAttribute('aria-expanded', 'false');
     }
   });
+}
+
+if (window.location.hash && selectableSections.some((section) => section.id === window.location.hash.slice(1))) {
+  selectSection(window.location.hash.slice(1));
 }
